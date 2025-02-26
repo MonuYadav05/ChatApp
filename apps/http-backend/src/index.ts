ce8772c6c304c86115b9c4d3059e7cb55e057019
Monu Yadav <monuyadav60010@gmail.com>
2025-02-27 00:43:58 +0530

    commit 2

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { CreateUserSchema, SigninSchema } from "@repo/common/types";
+import { db } from "@repo/db/client";
 const app = express();
 require('dotenv').config();
 
 app.post("/api/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "invalid inputs"
+        });
+        return;
+    }
     res.json({
         userId: 123,
     })
@@ -14,6 +23,13 @@ app.post("/api/signup", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "invalid inputs"
+        });
+        return;
+    }
     const userId = 123;
     const token = jwt.sign({ userId }, process.env.JWT_SECRET || "");
     res.json({
