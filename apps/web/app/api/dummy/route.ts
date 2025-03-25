caf91824b3a2f2cbceeaa5b0c428b32709c767ff
Monu Yadav <monuyadav60010@gmail.com>
2025-03-25 23:22:37 +0530

    delete dummy route

--- apps/web/app/api/dummy/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest, res: NextResponse) {
-    console.log(req.url)
-    return NextResponse.json({
-        message: "hello world"
-    })
-}
