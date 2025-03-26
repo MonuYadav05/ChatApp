38e0d9c98af98552ed075793ce20ae5d9dad1d46
Monu Yadav <monuyadav60010@gmail.com>
2025-03-26 12:10:34 +0530

    env

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(process.cwd(), "../../packages/database/.env") });
+
+const nextConfig = {
+    reactStrictMode: true,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,  // Ensure DATABASE_URL is available
+  },
+};
 
 export default nextConfig;
