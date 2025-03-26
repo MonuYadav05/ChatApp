120c6ce672fdb7017cdea6198ada3bfd46156742
Monu Yadav <monuyadav60010@gmail.com>
2025-03-27 02:29:44 +0530

    dvssdc

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://chat-backend-aon0.onrender.com/";
-export const WS_URL = "wss://chat-backend-aon0.onrender.com/";
+export const BACKEND_URL = "https://chat-backend-aon0.onrender.com";
+export const WS_URL = "wss://chat-backend-aon0.onrender.com";
